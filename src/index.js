b1665f8f21311f5b3430e62848f0ca9913f8572c
Hiro <sgi78ks@popcorn3.com>
2022-02-20 11:02:15 +0900

    map,filter

--- src/index.js
+++ src/index.js
@@ -77,3 +77,34 @@
 // console.log(num1);
 // console.log(num2);
 // console.log(...arr3);
+
+/**
+ *  mapやfilterを使った配列の処理
+ */
+
+const nameArr = ["鈴木", "国分寺", "神田"];
+
+//  nameArr.map((name)=>{
+//   console.log(name);
+//  });
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}さんです`));
+
+//  const nameArr2 = nameArr.map(name => `${name}さん`);
+//  console.log(nameArr2);
+
+// const numArr = [1,2,3,4,5];
+
+// const newNumArr = numArr.filter((num)=>{
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "鈴木") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
